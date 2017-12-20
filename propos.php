0bf235a7f4d54cbde3cfe2865d2e38c51fd082f4
Greninja <jean.blais@outlook.fr>
2017-12-19 21:39:52 -0500

    lecture des album pour vos photos, crée un album lors de l'inscrption

--- propos.php
+++ propos.php
@@ -14,22 +14,25 @@
 		<?php include "header.php" ?>
 		<div class="padding-left">
 			<h1>Mission</h1>
-			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.</p>
+			<p>Cette compagnie de photographie vise à faire ressorti vos moment marquant de votre en vous accompagant à trvaers ces évènements.
+            Nous offrons beaucoup de forfait personnalisée pour que vous pouvez choisir selon vos besoins et qui vous permetterez de ressortir
+            les moments fort de chacun de vos évènement.</p>
 		</div>
 		
 		<div class="padding-left">
 			<h1>Mes projets</h1>
 			<ul>
 				<li>Mariage</li>
-				<li>Projet 2</li>
-				<li>Projet 3</li>
+				<li>Photographie Aérienne</li>
+				<li>Photo de sport extreme</li>
 			</ul>
 		</div>
 		
 		<div class="padding-left">
 			<h1>Photographe</h1>
-			<img class="photographe" src="images/photographe.jpg">
-			<p>Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, </p>
+			<img class="photographe" src="images/alex.jpg">
+			<p>Je suis Alexandre Bruneau-Genesse, j'ai commencé mon entreprise, il y a 3 ans. Mon travail se démarque par la diversité des photographies
+            et de ma créativité pour créer un environnement propice à ressortir vos moments de gloire.</p>
 		</div>
 		
 		<?php include "footer.php" ?>
